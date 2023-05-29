bdfee380e07958b304ccdb6abd83126f55e4fb67
RaNdaHaddad <randahaddad8@gmail.com>
2023-05-29 13:40:59 +0200

    first commit

---
+++ client/src/Components/ProductList/ProductList.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+
+import ProductCard from '../ProductCard/ProductCard'
+import { getProducts } from '../../JS/Actions/ProductAction'
+
+const ProductList = () => {
+  const listProducts = useSelector((state) => state.ProductReducer.listProducts)
+
+  const load = useSelector((state) => state.ProductReducer.load)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getProducts)
+  }, [dispatch])
+  
+  return (
+    <div>
+{
+  load? (<h2>Loading...</h2>)
+  : listProducts? listProducts.map((el) => (
+
+    <ProductCard product={el} key={el._id} />
+    
+  )) 
+  : <h3>No products to display...</h3>
+}
+    </div>
+  )
+}
+
+export default ProductList
---
+++ client/src/JS/Reducers/index.js
@@ -0,0 +1,5 @@
+import AuthReducer from "./AuthReducer"
+import ProductReducer from "./ProductReducer"
+import {combineReducers} from 'redux'
+const rootReducer = combineReducers({AuthReducer, ProductReducer})
+export default rootReducer
